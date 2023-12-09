91540ddcc735d022b0d0a476ecf72d69b6e92462
Grace Lei <gracelei@Graces-MacBook-Pro.local>
2023-12-09 12:00:59 -0500

    debug

--- app/ug_country_types/page.tsx
+++ app/ug_country_types/page.tsx
@@ -3,25 +3,31 @@ import { Ug_Country_Type, columns } from "./columns"
 import { DataTable } from "./data-table"
 import { supabase } from '../../utils/supabaseClient'
 
-async function getData() {
-try {
-    let response = await supabase
-    .from('UG_COUNTRY_TYPES')
-    .select('*')
-    
-    if (response.error) {
-      // Log the error or handle it accordingly.
-      console.error('Error fetching data:', response.error);
-      throw response.error;
+
+
+async function getData() : Promise<Ug_Country_Type[]>{
+
+  try {
+    // Fetch data from Supabase
+    const { data, error } = await supabase
+        .from('UG_COUNTRY_TYPES')
+        .select('*');
+
+    if (error) {
+        // Handle Supabase API error
+        throw new Error(`Supabase API error: ${error.message}`);
     }
-    if (response.data) {
-        return response.data
+
+    // 'data' here is of type Ug_Country_Type[] | null
+    // You may want to handle the case where 'data' is null based on your app's logic
+    if (data) {
+        return data as Ug_Country_Type[];
+    } else {
+        throw new Error('Data not found');
     }
-    
   } catch (error) {
-    // Handle errors here
-    console.error('Error fetching data:', error);
-    throw error;
+      // Handle other errors
+      throw new Error(`Error fetching data: ${error}`);
   }
 }
 
